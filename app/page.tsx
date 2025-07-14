549d29252708563cce5f4d5682280340822ac665
JABRI Raed <jabriraed80@gmail.com>
2025-07-14 15:08:17 +0100

    create WorkManagement component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "@/components/Hero";
 import NavBar from "@/components/NavBar";
+import WorkManagement from "@/components/WorkManagement";
 
 export default function Home() {
   return (
     <main>
       <NavBar />
       <Hero />
+      <WorkManagement />
 
     </main>
   );
