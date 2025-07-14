b51b9ff04a652e8f7c246c8149d3165d4c6c366d
JABRI Raed <jabriraed80@gmail.com>
2025-07-14 12:31:52 +0100

    create page Hero tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import Hero from "@/components/Hero";
 import NavBar from "@/components/NavBar";
 
 export default function Home() {
   return (
     <main>
       <NavBar />
+      <Hero />
 
     </main>
   );
